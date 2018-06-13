843612027f161f06c03fb910a0b8cd09b438431c
Amol Aher <amolaher67@gmail.com>
2018-06-13 15:37:26 +0530

    Add project files.

---
+++ VMD.RESTApiResponseWrapper.Core/Extensions/ApiResponseMiddlewareExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace VMD.RESTApiResponseWrapper.Core.Extensions
+{
+    public static class ApiResponseMiddlewareExtension
+    {
+        public static IApplicationBuilder UseAPIResponseWrapperMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<APIResponseMiddleware>();
+        }
+    }
+}
---
+++ VMD.RESTApiResponseWrapper.Core/Wrappers/ResponseMessageEnum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace VMD.RESTApiResponseWrapper.Core.Wrappers
+{
+    public enum ResponseMessageEnum
+    {
+        [Description("Request successful.")]
+        Success,
+        [Description("Request responded with exceptions.")]
+        Exception,
+        [Description("Request denied.")]
+        UnAuthorized,
+        [Description("Request responded with validation error(s).")]
+        ValidationError,
+        [Description("Unable to process the request.")]
+        Failure
+    }
+}
